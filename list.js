055f6ca26ef1886bbd465e1a0c3181761fddbeef
Feri <farzad.dianat@gmail.com>
2024-10-19 21:12:18 -0700

    linked_list exercsise

--- list.js
+++ list.js
@@ -1,27 +1,66 @@
 function arrayToList(arr) {
-  let list = null;
-  for (let i = arr.length - 1; i >= 0; i--) {
-    let obj = { value: arr[i], pointer: list };
-    list = obj;
+  let list = { head: null, tail: null };
+
+  for (let i = 0; i < arr.length; i++) {
+    append(arr[i], list);
   }
   return list;
 }
 
 function listToArray(list) {
   let arr = [];
+  let current = list.head;
 
-  for (const key in list) {
-    if (key === "value") arr.push(list[key]);
-
-    if (typeof list[key] === "object" && list[key] !== null) {
-      listToArray(list[key]);
-    }
+  while (current !== null) {
+    arr.push(current.value);
+    current = current.pointer;
   }
 
   return arr;
 }
 
+function prepend(element, list) {
+  let newNode = { value: element, pointer: list.head };
+  list.head = newNode;
+}
+
+function append(element, list) {
+  let newNode = { value: element, pointer: null };
+
+  if (list.head === null) {
+    list.head = newNode;
+    list.tail = newNode;
+  } else {
+    list.tail.pointer = newNode;
+    list.tail = newNode;
+  }
+}
+
+function removeFirst(list) {
+  if (list === null) return;
+  list.head = list.head.pointer;
+  if (list.head === null) {
+    list.tail = null;
+  }
+}
+
+function nth(list, number) {
+  function recursive(count, current) {
+    if (current === null) return undefined;
+    if (count === number) return current;
+    return recursive(count + 1, current.pointer);
+  }
+  return recursive(0, list.head);
+}
+
 let myList = arrayToList([1, 2, 3]);
-let myArr = listToArray(myList);
+console.log(nth(myList, 1));
+/*
 console.log(myList);
+prepend(0, myList);
+console.log(myList);
+removeFirst(myList);
+console.log(myList);
+let myArr = listToArray(myList);
 console.log(myArr);
+*/
